f32dea08d9c271de06646d9532fd77d3a446a6d3
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-17 17:47:41 +0800

    Create ContactHubContext

---
+++ backend/ContactHubApi/Context/ContactHubContext.cs
@@ -0,0 +1,16 @@
+﻿using ContactHubApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactHubApi.Context
+{
+    public class ContactHubContext : DbContext
+    {
+        public ContactHubContext(DbContextOptions<ContactHubContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
