c7e58b6f4ff58d1e5bb0593a495a073ee6445512
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-17 17:47:28 +0800

    Create models

---
+++ backend/ContactHubApi/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactHubApi.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public ICollection<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
---
+++ backend/ContactHubApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactHubApi.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public ICollection<Contact> Contacts { get; set; } = new List<Contact>();
+    }
+}
