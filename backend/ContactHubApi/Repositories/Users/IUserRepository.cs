be8ccc17fa3f7ece01cc8ccb349f48b10c930fb0
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 12:33:30 +0800

    Create functions for user repository interface

--- backend/ContactHubApi/Repositories/Users/IUserRepository.cs
+++ backend/ContactHubApi/Repositories/Users/IUserRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace ContactHubApi.Repositories.Users
+﻿using ContactHubApi.Models;
+
+namespace ContactHubApi.Repositories.Users
 {
     public interface IUserRepository
     {
+        Task<Guid> CreateUser(User user);
+        Task<User?> GetUserById(Guid id);
+        Task<User?> GetUserByUsername(string username);
+        Task<bool> IsUserEmailExist(string email);
+        Task<bool> IsUsernameExist(string username);
+        Task<bool> UpdateUser(User user);
     }
 }
