051c9976d084e7bdb9330d55c86015671a5370df
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 17:40:30 +0800

    Create address repository

--- backend/ContactHubApi/Repositories/Addresses/IAddressRepository.cs
+++ backend/ContactHubApi/Repositories/Addresses/IAddressRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace ContactHubApi.Repositories.Addresses
+﻿using ContactHubApi.Models;
+
+namespace ContactHubApi.Repositories.Addresses
 {
     public interface IAddressRepository
     {
+        Task<Guid> CreateAddress(Address address);
+        Task<bool> DeleteAddress(Guid id);
+        Task<Address?> GetAddressById(Guid id);
+        Task<bool> UpdateAddress(Address address);
     }
 }
