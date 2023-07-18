eaeca811a40570417230b892496ee93bb69c4253
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 12:34:31 +0800

    Create UserMappings

---
+++ backend/ContactHubApi/Mappings/UserMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ContactHubApi.Dtos.Users;
+using ContactHubApi.Models;
+
+namespace ContactHubApi.Mappings
+{
+    public class UserMappings : Profile
+    {
+        public UserMappings()
+        {
+            CreateMap<UserCreationDto, User>();
+            CreateMap<User, UserDto>();
+            CreateMap<User, UserUIDetailsDto>();
+            CreateMap<User, UserTokenDto>();
+        }
+    }
+}
