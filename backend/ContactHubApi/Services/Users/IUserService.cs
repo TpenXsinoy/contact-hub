d597e0da056a4ac780ad5db009141a076c1090dc
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 12:33:58 +0800

    Create functions for user service interface

--- backend/ContactHubApi/Services/Users/IUserService.cs
+++ backend/ContactHubApi/Services/Users/IUserService.cs
@@ -1,6 +1,18 @@
-﻿namespace ContactHubApi.Services.Users
+﻿using ContactHubApi.Dtos.Users;
+using ContactHubApi.Models;
+
+namespace ContactHubApi.Services.Users
 {
     public interface IUserService
     {
+        Task<UserDto> CreateUser(UserCreationDto user);
+        public Task<UserUIDetailsDto?> GetUserById(Guid id);
+        public Task<UserUIDetailsDto?> GetUserByUsername(string username);
+        public Task<UserTokenDto?> GetUserByUsernameWithToken(string username);
+        Task<bool> IsUserEmailExist(string email);
+        Task<bool> IsUsernameExist(string username);
+        public Task<UserUIDetailsDto> UpdateUser(Guid id, UserCreationDto user);
+        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
     }
 }
