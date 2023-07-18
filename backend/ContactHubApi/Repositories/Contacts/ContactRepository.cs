077f1eb7427a361fb281cc33deb572be8e38c86e
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 12:34:57 +0800

    Create contacts repository

--- backend/ContactHubApi/Repositories/Contacts/ContactRepository.cs
+++ backend/ContactHubApi/Repositories/Contacts/ContactRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace ContactHubApi.Repositories.Contacts
+﻿using ContactHubApi.Models;
+
+namespace ContactHubApi.Repositories.Contacts
 {
     public class ContactRepository : IContactRepository
     {
+        public Task<Guid> CreateContact(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteContact(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<Contact>> GetAllContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contact?> GetContactById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateContact(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- backend/ContactHubApi/Repositories/Contacts/IContactRepository.cs
+++ backend/ContactHubApi/Repositories/Contacts/IContactRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ContactHubApi.Repositories.Contacts
+﻿using ContactHubApi.Models;
+
+namespace ContactHubApi.Repositories.Contacts
 {
-    public class IContactRepository
+    public interface IContactRepository
     {
+        Task<Guid> CreateContact(User user);
+        Task<Contact?> GetContactById(Guid id);
+        Task<IReadOnlyCollection<Contact>> GetAllContacts();
+        Task<bool> UpdateContact(Contact contact);
+        Task<bool> DeleteContact(Guid id);
     }
 }
