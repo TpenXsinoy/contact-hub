cfaff1f99c82d71bd664e473121c6b94dc1e0150
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 12:36:05 +0800

    Create Token services

---
+++ backend/ContactHubApi/Services/Tokens/ITokenService.cs
@@ -0,0 +1,28 @@
+﻿using ContactHubApi.Dtos.Users;
+using ContactHubApi.Models;
+
+namespace ContactHubApi.Services.Tokens
+{
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Creates a token for a user to use for authentication
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>Generated token</returns>
+        string CreateToken(UserTokenDto user);
+
+        /// <summary>
+        ///  Creates a refresh token for a user to use for authentication
+        /// </summary>
+        /// <returns>RefreshToken</returns>
+        RefreshToken GenerateRefreshToken();
+
+        /// <summary>
+        /// Verifies if token is still valid
+        /// </summary>
+        /// <param name="refreshToken"></param>
+        /// <returns> Valid, Invalid, or Expired</returns>
+        string Verify(string refreshToken, UserTokenDto user);
+    }
+}
