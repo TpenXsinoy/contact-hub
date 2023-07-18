c4c1bc0a7a3a7b2e439bb7b9773f42a75a3c2f4a
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-19 00:19:02 +0800

    Document context implementation

--- backend/ContactHubApi/Context/ContactHubContext.cs
+++ backend/ContactHubApi/Context/ContactHubContext.cs
@@ -5,9 +5,10 @@ namespace ContactHubApi.Context
 {
     public class ContactHubContext : DbContext
     {
-        public ContactHubContext(DbContextOptions<ContactHubContext> options) : base(options)
-        {
-        }
+        /// <summary>
+        /// Sets up the database context for the application using Entity Framework Core
+        /// </summary>
+        public ContactHubContext(DbContextOptions<ContactHubContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Contact> Contacts { get; set; }
