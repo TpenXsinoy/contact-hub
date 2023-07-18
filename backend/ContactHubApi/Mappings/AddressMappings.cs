1ca761cd52f0b8d97130b216c06f32e8ac68a7b0
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 17:40:47 +0800

    Create adress mappings

---
+++ backend/ContactHubApi/Mappings/AddressMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ContactHubApi.Dtos.Addresses;
+using ContactHubApi.Models;
+
+namespace ContactHubApi.Mappings
+{
+    public class AddressMappings : Profile
+    {
+        public AddressMappings()
+        {
+            CreateMap<AddressCreationDto, Address>();
+            CreateMap<Address, AddressDto>();
+        }
+    }
+}
