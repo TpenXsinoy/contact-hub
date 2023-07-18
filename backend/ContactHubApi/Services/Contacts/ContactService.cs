da5bea743f66c0e404a5e421503e674d8dd3b02f
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 15:31:31 +0800

    Implement contact services

--- backend/ContactHubApi/Services/Contacts/ContactService.cs
+++ backend/ContactHubApi/Services/Contacts/ContactService.cs
@@ -1,6 +1,59 @@
-﻿namespace ContactHubApi.Services.Contacts
+﻿using AutoMapper;
+using ContactHubApi.Dtos.Contacts;
+using ContactHubApi.Dtos.Users;
+using ContactHubApi.Models;
+using ContactHubApi.Repositories.Contacts;
+
+namespace ContactHubApi.Services.Contacts
 {
     public class ContactService : IContactService
     {
+        private readonly IContactRepository _contactRepository;
+        private readonly IMapper _mapper;
+
+        public ContactService(IContactRepository repository, IMapper mapper)
+        {
+            _contactRepository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<Contact> CreateContact(ContactCreationDto contact)
+        {
+            var contactModel = _mapper.Map<Contact>(contact);
+            contactModel.Id = await _contactRepository.CreateContact(contactModel);
+
+            return contactModel;
+        }
+
+        public async Task<bool> DeleteContact(Guid id)
+        {
+            return await _contactRepository.DeleteContact(id);
+        }
+
+        public async Task<IReadOnlyCollection<ContactDto>> GetAllContacts(Guid userId)
+        {
+            var contactModels = await _contactRepository.GetAllContacts(userId);
+            return _mapper.Map<IReadOnlyCollection<ContactDto>>(contactModels);
+        }
+
+        public async Task<ContactAddressDto?> GetContactById(Guid id)
+        {
+            var contactModel = await _contactRepository.GetContactById(id);
+            if (contactModel == null) return null;
+
+            return _mapper.Map<ContactAddressDto>(contactModel);
+        }
+
+        public async Task<ContactDto> UpdateContact(Guid id, ContactCreationDto contact)
+        {
+            var contactModel = _mapper.Map<Contact>(contact);
+            contactModel.Id = id;
+
+            await _contactRepository.UpdateContact(contactModel);
+            
+            var contactDto = _mapper.Map<ContactDto>(contactModel);
+
+            return contactDto;
+        }
     }
 }
--- backend/ContactHubApi/Services/Contacts/IContactService.cs
+++ backend/ContactHubApi/Services/Contacts/IContactService.cs
@@ -1,6 +1,14 @@
-﻿namespace ContactHubApi.Services.Contacts
+﻿using ContactHubApi.Dtos.Contacts;
+using ContactHubApi.Models;
+
+namespace ContactHubApi.Services.Contacts
 {
     public interface IContactService
     {
+        Task<Contact> CreateContact(ContactCreationDto contact);
+        Task<bool> DeleteContact(Guid id);
+        Task<IReadOnlyCollection<ContactDto>> GetAllContacts(Guid userId);
+        Task<ContactAddressDto?> GetContactById(Guid id);
+        Task<ContactDto> UpdateContact(Guid id, ContactCreationDto contact);
     }
 }
