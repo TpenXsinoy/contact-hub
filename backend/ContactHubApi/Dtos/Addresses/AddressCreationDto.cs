f5b31ae219f74079a1d9d5cb282d460d449faa66
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 17:41:06 +0800

    Create address dtos

---
+++ backend/ContactHubApi/Dtos/Addresses/AddressCreationDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactHubApi.Dtos.Addresses
+{
+    public class AddressCreationDto
+    {
+        [Required(ErrorMessage = "Address Type is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum lenghth for the address type is 50 characters.")]
+        public string AddressType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Street is required.")]
+        [MaxLength(100, ErrorMessage = "Maximum lenghth for the street is 100 characters.")]
+        public string Street { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum lenghth for the city is 50 characters.")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "State is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum lenghth for the state is 50 characters.")]
+        public string State { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Postal Code is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum lenghth for the postal code is 50 characters.")]
+        public string PostalCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Contact ID is required.")]
+        public Guid ContactId { get; set; }
+    }
+}
---
+++ backend/ContactHubApi/Dtos/Addresses/AddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ContactHubApi.Dtos.Addresses
+{
+    public class AddressDto
+    {
+        public Guid Id { get; set; }
+        public string AddressType { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+    }
+}
