0762e093787489175eef632339c7628efcbfe3ca
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 15:31:46 +0800

    Update contact model

--- backend/ContactHubApi/Models/Contact.cs
+++ backend/ContactHubApi/Models/Contact.cs
@@ -6,5 +6,6 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public ICollection<Address> Addresses { get; set; } = new List<Address>();
+        public Guid UserId { get; set; }
     }
 }
