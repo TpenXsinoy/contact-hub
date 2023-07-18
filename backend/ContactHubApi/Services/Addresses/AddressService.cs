10976a34e1aeb9115ad6f400219153b873221e62
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 17:40:39 +0800

    Create address service

--- backend/ContactHubApi/Services/Addresses/AddressService.cs
+++ backend/ContactHubApi/Services/Addresses/AddressService.cs
@@ -1,6 +1,51 @@
-﻿namespace ContactHubApi.Services.Addresses
+﻿using AutoMapper;
+using ContactHubApi.Dtos.Addresses;
+using ContactHubApi.Models;
+using ContactHubApi.Repositories.Addresses;
+
+namespace ContactHubApi.Services.Addresses
 {
     public class AddressService : IAddressService
     {
+        private readonly IAddressRepository _addressRepository;
+        private readonly IMapper _mapper;
+
+        public AddressService(IAddressRepository repository, IMapper mapper)
+        {
+            _addressRepository = repository;
+            _mapper = mapper;
+        }
+        public async Task<Address> CreateAddress(AddressCreationDto address)
+        {
+            var addressModel = _mapper.Map<Address>(address);
+            addressModel.Id = await _addressRepository.CreateAddress(addressModel);
+
+            return addressModel;
+        }
+
+        public async Task<bool> DeleteAddress(Guid id)
+        {
+            return await _addressRepository.DeleteAddress(id);
+        }
+
+        public async Task<AddressDto?> GetAddressById(Guid id)
+        {
+            var addressModel = await _addressRepository.GetAddressById(id);
+            if (addressModel == null) return null;
+
+            return _mapper.Map<AddressDto>(addressModel);
+        }
+
+        public async Task<AddressDto> UpdateAddress(Guid id, AddressCreationDto address)
+        {
+            var addressModel = _mapper.Map<Address>(address);
+            addressModel.Id = id;
+
+            await _addressRepository.UpdateAddress(addressModel);
+
+            var addressDto = _mapper.Map<AddressDto>(addressModel);
+
+            return addressDto;
+        }
     }
 }
--- backend/ContactHubApi/Services/Addresses/IAddressService.cs
+++ backend/ContactHubApi/Services/Addresses/IAddressService.cs
@@ -1,6 +1,13 @@
-﻿namespace ContactHubApi.Services.Addresses
+﻿using ContactHubApi.Dtos.Addresses;
+using ContactHubApi.Models;
+
+namespace ContactHubApi.Services.Addresses
 {
     public interface IAddressService
     {
+        Task<Address> CreateAddress(AddressCreationDto address);
+        Task<bool> DeleteAddress(Guid id);
+        Task<AddressDto?> GetAddressById(Guid id);
+        Task<AddressDto> UpdateAddress(Guid id, AddressCreationDto address);
     }
 }
