034ed2af3e7166f959b46532c759e0eea4da1c35
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-18 15:30:44 +0800

    Create Contact Dtos

---
+++ backend/ContactHubApi/Dtos/Contacts/ContactDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactHubApi.Dtos.Contacts
+{
+    public class ContactDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
