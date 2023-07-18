1bb30a760dec7495f638f99805170bfec995ee10
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-19 00:18:41 +0800

    Document tokens controller

--- backend/ContactHubApi/Controllers/TokensController.cs
+++ backend/ContactHubApi/Controllers/TokensController.cs
@@ -1,4 +1,5 @@
-﻿using ContactHubApi.Dtos.Tokens;
+﻿using System.Diagnostics.CodeAnalysis;
+using ContactHubApi.Dtos.Tokens;
 using ContactHubApi.Dtos.Users;
 using ContactHubApi.Models;
 using ContactHubApi.Services.Tokens;
@@ -13,7 +14,7 @@ namespace ContactHubApi.Controllers
     public class TokensController : ControllerBase
     {
         private readonly ILogger<TokensController> _logger;
-        private static UserTokenDto _user = new();
+        private static UserTokenDto? _user = new();
         private readonly ITokenService _tokenService;
         private readonly IUserService _userService;
 
@@ -27,16 +28,42 @@ namespace ContactHubApi.Controllers
             _userService = takerService;
         }
 
+        /// <summary>
+        /// Generates an access token
+        /// </summary>
+        /// <param name="request">The user to be authenticated with a token</param>
+        /// <returns>Returns an access token</returns>
+        /// <remarks>
+        /// Sample Request:
+        ///     
+        ///     POST /api/tokens/acquire
+        ///     {
+        ///         "username": "jhondoe",
+        ///         "password": "jhondoe123"
+        ///     }
+        ///    
+        /// </remarks>
+        /// 
+        /// <response code="200">Successfully acquired an access token</response>
+        /// <response code="400">Details are invalid</response>
+        /// <response code="404">User is not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpPost("acquire")]
         [AllowAnonymous]
         [Consumes("application/json")]
         [ProducesResponseType(typeof(TokenDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> AcquireToken([FromBody] UserLoginDto userLogin)
+        public async Task<IActionResult> AcquireToken([FromBody] UserLoginDto request)
         {
             try
             {
-                _user = await _userService.GetUserByUsernameWithToken(userLogin.Username);
+                _user = await _userService.GetUserByUsernameWithToken(request.Username!);
+
+                if (_user == null)
+                {
+                    return NotFound($"User {request.Username} is not found");
+                }
 
                 string accessToken = _tokenService.CreateToken(_user!);
                 var refreshToken = _tokenService.GenerateRefreshToken();
@@ -57,6 +84,26 @@ namespace ContactHubApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Renews an access token and refresh token
+        /// </summary>
+        /// <param name="request">Refresh token to be verified</param>
+        /// <returns>Returns TokenDto, an access token and refresh token</returns>
+        /// <remarks>
+        /// Sample Request:
+        ///     
+        ///     POST /api/tokens/renew
+        ///     {
+        ///         "username": "jhondoe",
+        ///         "password": "jhondoe123"
+        ///     }
+        ///    
+        /// </remarks>
+        /// 
+        /// <response code="200">Successfully acquired an access token</response>
+        /// <response code="400">Details are invalid</response>
+        /// <response code="404">User is not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpPost("renew")]
         [AllowAnonymous]
         [Produces("application/json")]
@@ -65,7 +112,7 @@ namespace ContactHubApi.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult RenewToken([FromBody] RenewTokenDto request)
         {
-            string tokenStatus = _tokenService.Verify(request.RefreshToken, _user);
+            string tokenStatus = _tokenService.Verify(request.RefreshToken, _user!);
 
             if (tokenStatus == "Invalid")
             {
@@ -74,10 +121,9 @@ namespace ContactHubApi.Controllers
             else if (tokenStatus == "Expired")
             {
                 return Unauthorized("Refresh token expired");
-
             }
 
-            string accessToken = _tokenService.CreateToken(_user);
+            string accessToken = _tokenService.CreateToken(_user!);
             var newRefreshToken = _tokenService.GenerateRefreshToken();
             SetRefreshToken(newRefreshToken);
 
@@ -90,11 +136,15 @@ namespace ContactHubApi.Controllers
             return Ok(tokenDto);
         }
 
-        private static void SetRefreshToken(RefreshToken newRefreshToken)
+        /// <summary>
+        /// Sets the refresh token to the user
+        /// </summary>
+        /// <param name="refreshToken">Refresh token to be set</param>
+        private static void SetRefreshToken(RefreshToken refreshToken)
         {
-            _user.RefreshToken = newRefreshToken.Token;
-            _user.TokenCreated = newRefreshToken.Created;
-            _user.TokenExpires = newRefreshToken.Expires;
+            _user!.RefreshToken = refreshToken.Token;
+            _user.TokenCreated = refreshToken.Created;
+            _user.TokenExpires = refreshToken.Expires;
         }
     }
 }
