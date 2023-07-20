7b08d9250731ecc5a61922c949393ac154561752
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-20 17:02:04 +0800

    Crteate contact mappings tests

---
+++ backend/ContactHubApiTests/Mappings/ContactMappingsTests.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using ContactHubApi.Dtos.Contacts;
+using ContactHubApi.Mappings;
+using ContactHubApi.Models;
+using Moq;
+
+namespace ContactHubApiTests.Mappings
+{
+    public class ContactMappingsTests
+    {
+        private readonly IMapper _mapper;
+        private readonly MapperConfiguration _configuration;
+
+        public ContactMappingsTests()
+        {
+            _configuration = new MapperConfiguration(cfg => cfg.AddProfile<ContactMappings>());
+            _mapper = _configuration.CreateMapper();
+        }
+
+        // ContactCreationDto to Contact
+        [Fact]
+        public void Map_ValidContactCreationDto_ReturnsContact()
+        {
+            //Arrange
+            var contactCreationDto = new ContactCreationDto
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                UserId = It.IsAny<Guid>(),
+            };
+
+            var contact = new Contact
+            {
+                Id = It.IsAny<Guid>(),
+                FirstName = contactCreationDto.FirstName,
+                LastName = contactCreationDto.LastName,
+                Addresses = new List<Address>(),
+                UserId = contactCreationDto.UserId
+            };
+
+            //Act
+            var result = _mapper.Map<Contact>(contactCreationDto);
+
+            //Assert
+            Assert.Equal(contact.Id, result.Id);
+            Assert.Equal(contact.FirstName, result.FirstName);
+            Assert.Equal(contact.LastName, result.LastName);
+            Assert.Equal(contact.Addresses, result.Addresses);
+            Assert.Equal(contact.UserId, result.UserId);
+        }
+
+        // Contact to ContactAddressDto
+        [Fact]
+        public void Map_ValidContact_ReturnsContactAddressDto()
+        {
+            //Arrange
+            var contact = new Contact
+            {
+                Id = It.IsAny<Guid>(),
+                FirstName = "John",
+                LastName = "Doe",
+                Addresses = new List<Address>(),
+                UserId = It.IsAny<Guid>()
+            };
+
+            var contactAddressDto = new ContactAddressDto
+            {
+                Id = contact.Id,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+                Addresses = contact.Addresses
+            };
+
+            //Act
+            var result = _mapper.Map<ContactAddressDto>(contact);
+
+            //Assert
+            Assert.Equal(contactAddressDto.Id, result.Id);
+            Assert.Equal(contactAddressDto.FirstName, result.FirstName);
+            Assert.Equal(contactAddressDto.LastName, result.LastName);
+            Assert.Equal(contactAddressDto.Addresses, result.Addresses);
+        }
+
+        // Contact to ContactDto
+        [Fact]
+        public void Map_ValidContact_ReturnsContactDto()
+        {
+            //Arrange
+            var contact = new Contact
+            {
+                Id = It.IsAny<Guid>(),
+                FirstName = "John",
+                LastName = "Doe",
+                Addresses = new List<Address>(),
+                UserId = It.IsAny<Guid>()
+            };
+
+            var contactDto = new ContactDto
+            {
+                Id = contact.Id,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+            };
+
+            //Act
+            var result = _mapper.Map<ContactDto>(contact);
+
+            //Assert
+            Assert.Equal(contactDto.Id, result.Id);
+            Assert.Equal(contactDto.FirstName, result.FirstName);
+            Assert.Equal(contactDto.LastName, result.LastName);
+        }
+    }
+}
