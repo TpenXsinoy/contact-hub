e740ab359563b8e2e9cefb612507d674e5c86951
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-21 22:29:42 +0800

    Add strong password attribute for validation

---
+++ backend/ContactHubApi/CustomValidations/StrongPasswordAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactHubApi.Utils
+{
+    public class StrongPasswordAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null || !(value is string password))
+            {
+                return false;
+            }
+
+            // Password must have at least 8 characters
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            // Password must have at least 1 numeric character
+            if (!password.Any(char.IsDigit))
+            {
+                return false;
+            }
+
+            // Password must have at least 1 lowercase character
+            if (!password.Any(char.IsLower))
+            {
+                return false;
+            }
+
+            // Password must have at least 1 uppercase character
+            if (!password.Any(char.IsUpper))
+            {
+                return false;
+            }
+
+            // Password must have at least 1 special character (non-alphanumeric)
+            if (!password.Any(c => !char.IsLetterOrDigit(c)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
