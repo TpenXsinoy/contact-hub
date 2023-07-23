67a6c2137f5af4d673946ad6407141e068eded17
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-24 02:08:09 +0800

    Export screens

---
+++ frontend/src/screens/user/index.js
@@ -0,0 +1,7 @@
+export { default as Contacts } from "./Contacts";
+export { default as CreateContact } from "./CreateContact";
+export { default as UpdateContact } from "./UpdateContact";
+export { default as ViewContact } from "./ViewContact";
+export { default as CreateAddress } from "./CreateAddress";
+export { default as UpdateAddress } from "./UpdateAddress";
+export { default as Settings } from "./Settings";
