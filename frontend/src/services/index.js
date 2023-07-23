d86ead1fefb0c33f8a687ab5cc8f288f22b3d0ef
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-24 02:04:49 +0800

    Export services

---
+++ frontend/src/services/index.js
@@ -0,0 +1,4 @@
+export { default as AddressesService } from "./AddressesService";
+export { default as ContactsService } from "./ContactsService";
+export { default as UsersService } from "./UsersService";
+export { default as TokensService } from "./TokensService";
