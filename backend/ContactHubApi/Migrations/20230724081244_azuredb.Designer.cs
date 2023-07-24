b82baeaa1c09bdc24e9154cae72ef33a6cb6b781
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-24 16:23:45 +0800

    Add deployment files

--- backend/ContactHubApi/Migrations/20230724081244_azuredb.Designer.cs
+++ backend/ContactHubApi/Migrations/20230724081244_azuredb.Designer.cs
@@ -4,6 +4,7 @@ using ContactHubApi.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContactHubApi.Migrations
 {
     [DbContext(typeof(ContactHubContext))]
-    partial class ContactHubContextModelSnapshot : ModelSnapshot
+    [Migration("20230724081244_azuredb")]
+    partial class azuredb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
