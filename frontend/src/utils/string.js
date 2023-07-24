13e46c48b230e42adcbb26d2df634f5ca9718fde
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-24 21:21:36 +0800

    Rearrange and put comments

--- frontend/src/utils/string.js
+++ frontend/src/utils/string.js
@@ -1,15 +1,18 @@
+// Checks if an email address is valid
 export const isValidEmail = (email) => {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 };
 
+// Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character
 export const isValidPassword = (password) => {
   const re =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   return re.test(password);
 };
 
+// Gets the username from an email address (everything before the @)
 export const getEmailUserName = (email) => {
   const parts = email.split("@");
   return parts[0];
