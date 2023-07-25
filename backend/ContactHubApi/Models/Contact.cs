fda93af81dd2c239067c609a64bcb917acdcbbeb
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-25 15:33:17 +0800

    Add phone number property

--- backend/ContactHubApi/Models/Contact.cs
+++ backend/ContactHubApi/Models/Contact.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
         public ICollection<Address> Addresses { get; set; } = new List<Address>();
         public Guid UserId { get; set; }
     }
