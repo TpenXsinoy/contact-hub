62adf9d3f228d6db692d55ab4b5c7fbe65005b31
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-25 15:38:42 +0800

    Remove PageNotFound

--- frontend/src/screen-wrappers/LoginSignup/index.jsx
+++ frontend/src/screen-wrappers/LoginSignup/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Switch, Redirect } from "react-router-dom";
 import NoAuthRoute from "src/hocs/NoAuthRoute";
-import { Login, PageNotFound, SignUp } from "src/screens/public";
+import { Login, SignUp } from "src/screens/public";
 
 import styles from "./styles.module.scss";
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
 
 const LoginSignupContainer = () => {
   return (
@@ -24,8 +23,6 @@ const LoginSignupContainer = () => {
             exact
             render={(props) => <SignUp {...props} />}
           />
-
-          <Route component={PageNotFound} />
 
           <Redirect from="*" to="/login" />
         </Switch>
