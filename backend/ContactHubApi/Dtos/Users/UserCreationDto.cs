49a856d42e61fce06f055f70147540282ff281c8
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-27 09:33:31 +0800

    Add minimum length validation

--- backend/ContactHubApi/Dtos/Users/UserCreationDto.cs
+++ backend/ContactHubApi/Dtos/Users/UserCreationDto.cs
@@ -7,10 +7,12 @@ namespace ContactHubApi.Dtos.Users
     {
         [Required(ErrorMessage = "First Name is required.")]
         [MaxLength(50, ErrorMessage = "Maximum lenghth for the first name is 50 characters.")]
+        [MinLength(2, ErrorMessage = "Minimum lenghth for the first name is 2 characters.")]
         public string FirstName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Last Name is required.")]
         [MaxLength(50, ErrorMessage = "Maximum lenghth for the last name is 50 characters.")]
+        [MinLength(2, ErrorMessage = "Minimum lenghth for the last name is 2 characters.")]
         public string LastName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email is required."), EmailAddress(ErrorMessage = "Email is invalid.")]
@@ -19,6 +21,7 @@ namespace ContactHubApi.Dtos.Users
 
         [Required(ErrorMessage = "Username is required.")]
         [MaxLength(50, ErrorMessage = "Maximum lenghth for the username is 50 characters.")]
+        [MinLength(5, ErrorMessage = "Minimum lenghth for the username is 5 characters.")]
         public string Username { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password is required.")]
