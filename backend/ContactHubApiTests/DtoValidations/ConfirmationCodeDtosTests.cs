6023d7bc63e5b3cd67fff1f0164bc2610d9e5328
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-27 10:03:02 +0800

    Create confirmation code dtos tests

---
+++ backend/ContactHubApiTests/DtoValidations/ConfirmationCodeDtosTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactHubApi.Dtos.Addresses;
+using ContactHubApi.Dtos.Email;
+using ContactHubApi.Dtos.Users;
+
+namespace ContactHubApiTests.DtoValidations
+{
+    public class ConfirmationCodeDtosTests
+    {
+        // EmailConfirmationCodeDto Tests
+        [Fact]
+        public void ValidEmailConfirmationCodeDto_NoValidationErrors()
+        {
+            // Arrange
+            var emailConfirmationCodeDto = new EmailConfirmationCodeDto
+            {
+                Email = "john@gmail.com",
+                Code = "123456"
+            };
+
+            var validationContext = new ValidationContext(emailConfirmationCodeDto, null, null);
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool isValid = Validator.TryValidateObject(emailConfirmationCodeDto, validationContext, validationResults, true);
+
+            // Assert
+            Assert.True(isValid);
+            Assert.Empty(validationResults);
+        }
+
+        [Theory]
+        [InlineData("", "123456", "Email is required.")]
+        [InlineData("john@gmail.com", "", "Code is required.")]
+        [InlineData("johngmail.com", "123456", "Email is invalid.")]
+        public void InvalidEmailConfirmationCodeDto_ValidationErrors(string email, string code, string expectedErrorMessage)
+        {
+            // Arrange
+            var emailConfirmationCodeDto = new EmailConfirmationCodeDto
+            {
+                Email = email,
+                Code = code
+            };
+
+            var validationContext = new ValidationContext(emailConfirmationCodeDto, null, null);
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool isValid = Validator.TryValidateObject(emailConfirmationCodeDto, validationContext, validationResults, true);
+
+            // Assert
+            Assert.False(isValid);
+            Assert.Contains(validationResults, validationResult => validationResult.ErrorMessage == expectedErrorMessage);
+        }
+
+        // EmailDto Tests
+        [Fact]
+        public void ValidEmailDto_NoValidationErrors()
+        {
+            // Arrange
+            var emailDto = new EmailDto
+            {
+                To = "john@gmail.com",
+                Body = "123456"
+            };
+
+            var validationContext = new ValidationContext(emailDto, null, null);
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool isValid = Validator.TryValidateObject(emailDto, validationContext, validationResults, true);
+
+            // Assert
+            Assert.True(isValid);
+            Assert.Empty(validationResults);
+        }
+
+        [Theory]
+        [InlineData("", "test", "Email is required.")]
+        [InlineData("john@gmail.com", "", "Body is required.")]
+        [InlineData("johngmail.com", "test", "Email is invalid.")]
+        public void InvalidEmailDto_ValidationErrors(string to, string body, string expectedErrorMessage)
+        {
+            // Arrange
+            var emailDto = new EmailDto
+            {
+                To = to,
+                Body = body
+            };
+
+            var validationContext = new ValidationContext(emailDto, null, null);
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool isValid = Validator.TryValidateObject(emailDto, validationContext, validationResults, true);
+
+            // Assert
+            Assert.False(isValid);
+            Assert.Contains(validationResults, validationResult => validationResult.ErrorMessage == expectedErrorMessage);
+        }
+    }
+}
