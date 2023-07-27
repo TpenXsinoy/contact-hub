7eaf71b654fb340c6ce433a96e90756818a715da
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-27 10:02:39 +0800

    Create email confitmation code dto

---
+++ backend/ContactHubApi/Dtos/ConfirmationCode/EmailConfirmationCodeDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactHubApi.Dtos.Email
+{
+    public class EmailConfirmationCodeDto
+    {
+        [Required(ErrorMessage = "Email is required."), EmailAddress(ErrorMessage = "Email is invalid.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Code is required.")]
+        public string Code { get; set; } = string.Empty;
+    }
+}
