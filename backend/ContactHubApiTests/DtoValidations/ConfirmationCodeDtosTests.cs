0461f1accc39ade60d54379819b3396743796db5
Stephine Sinoy <ssinoy@fullscale.io>
2023-07-27 11:25:26 +0800

    Remove unnecessary usings

--- backend/ContactHubApiTests/DtoValidations/ConfirmationCodeDtosTests.cs
+++ backend/ContactHubApiTests/DtoValidations/ConfirmationCodeDtosTests.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ContactHubApi.Dtos.Addresses;
+﻿using System.ComponentModel.DataAnnotations;
 using ContactHubApi.Dtos.Email;
-using ContactHubApi.Dtos.Users;
 
 namespace ContactHubApiTests.DtoValidations
 {
